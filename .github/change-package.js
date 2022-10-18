3ac6f2b61835f419b9457a626017cb55d0e4f9ae
Evgeny Kruglov <ekruglov@gmail.com>
2022-10-18 12:59:13 +0200

    ci: update README and repository url

--- .github/change-package.js
+++ .github/change-package.js
@@ -1,5 +1,6 @@
 'use strict';
-// Do not forget to run `npm config set access public`
+// Do not forget to run `npm config set access public` before first local publish
+// Allow read-and-write permissons for Github Actions: repo->settings->actions->general
 const fs = require('fs');
 const packageFile = JSON.parse(fs.readFileSync('./package.json'));
 const name = process.env['GITHUB_REPOSITORY']?.split('/');
@@ -8,4 +9,8 @@ if (!name?.[1]) {
     process.exit(1);
 }
 packageFile.name = '@umpacken/' + name[1];
+if (packageFile.repository?.type === 'git') {
+    packageFile.repository.url = 'git://github.com/' + process.env['GITHUB_REPOSITORY'] + '.git';
+}
+
 fs.writeFileSync('./package.json', JSON.stringify(packageFile, null, 2));
